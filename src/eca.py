ad6b31b8425ba0c61b54073f14d4520ef10d1aab
KlutzJump <64670342+KlutzJump@users.noreply.github.com>
2022-04-23 10:28:06 -0700

    Create eca.py
    main script file

---
+++ src/eca.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import calc
+import util
+
+
+"""
+pyeca runs elementary cellular automata in the terminal
+
+use:
+    syntax:
+        pyeca [rule] [cols] [steps]
+
+    args:
+        rule: rule specified using Wolfram notation (1-256)
+        cols: character width of automaton
+        steps: number of steps/iterations to compute
+
+    optional flags:
+        --initial or -i: initial state specified with 1s and 0s
+        --out or -o: character (1s) used as output to terminal
+
+"""
+
+
+if __name__ == "__main__":
+    args = util.parse_args()
+
+    if args.initial:
+        util.check_initial_state(args.initial, args.cols)
+        initial_state = args.initial
+    else:
+        initial_state = util.random_initial_state(args.cols)
+
+    if args.out:
+        util.check_symbol(args.out)
+        symbol = args.out
+    else:
+        symbol = "\u2588"
+
+    universe = calc.run(initial_state, args.rule, args.steps)
+    util.print_universe(universe, symbol)
